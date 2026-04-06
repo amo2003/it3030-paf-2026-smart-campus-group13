ada5fc47e2a8564a4cb10895c5e1474339c46f75
Amod Indupa <amodindupa9@gmail.com>
2026-04-06 10:16:17 +0530

    add

--- backend/backend/src/main/java/backend/Module_2/Exception/GlobalExceptionsHandler.java
+++ backend/backend/src/main/java/backend/Module_2/Exception/GlobalExceptionsHandler.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionsHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, Object>> handleRuntimeException(RuntimeException ex) {
