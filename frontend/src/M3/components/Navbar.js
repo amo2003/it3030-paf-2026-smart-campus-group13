79b6a6739a63d8a761b94e722d6bb348060f29f5
Amod Indupa <amodindupa9@gmail.com>
2026-04-23 23:55:50 +0530

    Update Navbar.js

--- frontend/src/M3/components/Navbar.js
+++ frontend/src/M3/components/Navbar.js
@@ -1,17 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <h2>Maintenance Ticketing</h2>
-        <span>Module 3</span>
-      </div>
-
-      <div className="nav-links">
-        <Link to="/">Dashboard</Link>
-        <Link to="/tickets/create">Create Ticket</Link>
+    <nav className="m3-navbar">
+      <div className="m3-navbar-inner">
+        <div className="m3-navbar-brand">
+          <h2>🎫 Maintenance Ticketing</h2>
+          <span>Module 3</span>
+        </div>
+        <div className="m3-nav-links">
+          <Link to="/tickets" className="m3-nav-link">Dashboard</Link>
+          *<Link to="/tickets/create" className="m3-nav-link primary">+ Create Ticket</Link>*
+        </div>
       </div>
     </nav>
   );
